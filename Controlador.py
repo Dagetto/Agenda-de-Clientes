1131355ddf1e8080829cfd7edcfc0bb0b6dfddd7
Lucas Monzón <121673252+Dagetto@users.noreply.github.com>
2023-08-31 18:26:24 +0200

    Update Controlador.py

--- Controlador.py
+++ Controlador.py
@@ -1,16 +1,21 @@
 from tkinter import Tk
-import Vista
+import vista
+import observador
 
 
 class ControladorPoo:
     def __init__(self, root):
 
         self.root_controler = root
-        self.objeto_vista = Vista.ventana_interfaz(
+        self.objeto_vista = vista.VistaPoo(
             self.root_controler
         )
+        self.objeto_observador = observador.ConcreteObserverA(
+            self.objeto_vista.objeto2_basedatos
+        )
 
-    if __name__ == "__main__":
-        main = Tk()
-        objeto1_controlador = Vista.VistaPoo(main)
-        main.mainloop()
+
+if __name__ == "__main__":
+    main = Tk()
+    application = ControladorPoo(main)
+    main.mainloop()
